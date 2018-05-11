9af1958762d238b7b1331aa601c3723fa153cf0f
Fandi Ahmad <slazeryy@gmail.com>
2018-05-11 08:35:31 +0700

    FAQ & Jadwal Template

--- resources/views/frontend/pages/jadwal.blade.php
+++ resources/views/frontend/pages/jadwal.blade.php
@@ -1,1 +1,111 @@
-jadwal
+@extends('frontend.layouts')
+@section('title')
+Jadwal Pelaksanaan | KMIPN 2018
+@endsection
+@section('content')
+  <div class="container default-container">
+    <div class="panel panel-default panel-custom">
+      <div class="panel-heading">
+        <h4>Jadwal Pelaksanaan KMIPN 2018</h4>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead style="font-size:13px;" class="centered">
+            <tr>
+              <th>Kategori Lomba</th>
+              <th>Batas Pendaftaram</th>
+              <th>Babak Penyisihan 1</th>
+              <th>Pengumuman Babak 2</th>
+              <th>Babak Penyisihan 2</th>
+              <th>Pengumuman Lolos Final</th>
+              <th>Babak Final</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Cipta Inovasi</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>Bisnis TIK</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>Game Development</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>E-Goverment</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>IoT</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>Desain Animasi</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>Keamanan Jaringan</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+            <tr>
+              <td>Hackathon</td>
+              <td>17 Juli 2018</td>
+              <td>19 Juli 2018</td>
+              <td>30 September 2018</td>
+              <td>3 Oktober 2018</td>
+              <td>10 Oktober</td>
+
+              <td>15 Oktober 2018</td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  @endsection
